396332f603d2fe62975b7818b0bf10a6cc56d845
beqab <beqab@LAPTOP>
2024-05-10 22:21:56 +0400

    add image optimizer

---
+++ ImageOptimizer/Program.cs
@@ -0,0 +1,34 @@
+﻿using ImageMagick;
+
+var location = "";
+
+Console.WriteLine("start");
+
+Parallel.ForEach(Directory.GetFiles(location, "*.jpg", SearchOption.AllDirectories), filePath =>
+{
+    if (!filePath.EndsWith(".webp"))
+        try
+        {
+            using var image = new MagickImage(filePath) { Quality = 85 };
+            image.Write($"{filePath}.webp", MagickFormat.WebP);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    else
+        try
+        {
+            using var image = new MagickImage(filePath);
+            image.Write(filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+});
+
+Console.Write("done, click enter to exit: ");
+
+
+Console.ReadLine();
